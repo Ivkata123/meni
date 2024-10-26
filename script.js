a3195ccc621d241f3c5cbeec5b10d361c65d939a
Ivkata123 <126425494+Ivkata123@users.noreply.github.com>
2024-10-26 18:37:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function changeLanguage(lang) {
     const elements = document.querySelectorAll('[data-en]');
     elements.forEach(el => {
-        el.textContent = el.getAttribute(`data-${lang}`);
+        const span = el.querySelector('span');
+        if (span) {
+            span.textContent = span.getAttribute(`data-${lang}`);
+        }
     });
 }
