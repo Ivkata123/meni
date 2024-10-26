343d36318dace407ebe0abffd768cdc1815ca211
Ivkata123 <126425494+Ivkata123@users.noreply.github.com>
2024-10-26 18:43:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function changeLanguage(lang) {
     const elements = document.querySelectorAll('[data-en]');
     elements.forEach(el => {
+        
+        if (el.hasAttribute(`data-${lang}`)) {
+            el.textContent = el.getAttribute(`data-${lang}`);
+        }
+
+        
         const span = el.querySelector('span');
         if (span) {
             span.textContent = span.getAttribute(`data-${lang}`);
